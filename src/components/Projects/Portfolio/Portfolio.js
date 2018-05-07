160325a2945041b6431f662d8a7a351e745ad8ed
Patrick Knight <ptknight00@gmail.com>
2018-05-07 18:14:47 -0500

    adding content

--- src/components/Projects/Portfolio/Portfolio.js
+++ src/components/Projects/Portfolio/Portfolio.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import './Portfolio.css';
 
-
 class Portfolio extends Component {
+    state = {
 
-    componentDidMount() {
-        
     }
-    render() {
-        return (
-            <div id='portfolio'>
-                {/* put projects here */}
-                
-            </div>
-        )
-    }
-};
+	componentDidMount() {}
+	render() {
+		return (
+			<div id="portfolio">
+				<ul className="open-games">
+					{/* {this.props.gameList.map(game => (
+						<li key={game.name} data-name={game.name} className="open-game" onClick={this.joinGame}>
+							{game.name} - {game.status}
+						</li>
+					))} */}
+				</ul>
+			</div>
+		);
+	}
+}
 
 export default Portfolio;
